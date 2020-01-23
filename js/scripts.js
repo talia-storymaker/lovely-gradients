16780274bb5d0441275b07db29bcc47d703d3ea6
talia-storymaker <storymakerechidna@gmail.com>
2020-01-22 22:16:19 -0600

    Trying by accessing it as a property

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 function populateGradientCodes() {
     const gradientContainers = document.querySelectorAll('.gradient-container');
     for (let i = 0; i < gradientContainers.length; i++) {
-        gradientContainers[i].querySelector('.gradient-code').textContent = window.getComputedStyle(gradientContainers[i].querySelector('.gradient')).getPropertyValue('background');
+        gradientContainers[i].querySelector('.gradient-code').textContent = window.getComputedStyle(gradientContainers[i].querySelector('.gradient')).background;
+        console.log(window.getComputedStyle(gradientContainers[i].querySelector('.gradient')).background);
     }
 }
 
